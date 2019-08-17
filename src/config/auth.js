810d5cf5bf83dd4418e813d267cf782ff9e6528a
Pedro Ernesto Perdoná Rech <p.perdona@gmail.com>
2019-08-17 10:15:47 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: process.env.APP_SECRET,
+  expiresInd: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Mailtrap (ambient dev only)
+ * AmazonSES
+ * Mailgun
+ * Sparkpost
+ */
